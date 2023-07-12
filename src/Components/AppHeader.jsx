355b22c098102f4aea234cc28be95d8a035c73de
scottmotion <scott@scottmotion.com>
2023-07-12 16:54:02 -0700

    logo

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -1,9 +1,9 @@
 export default function AppHeader() {
     return (
       <header className="header">
-        <div className="header__app-brand">App Brand</div>
+        <div className="header__app-brand"><img className="header__app-logo" /></div>
         <div className="header__nav">
-          <div className="header__board-title">Board Title</div>
+          <h1 className="header__board-title">Board Title</h1>
           <button>Button</button>
         </div>
       </header>
