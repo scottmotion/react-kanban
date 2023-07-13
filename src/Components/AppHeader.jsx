f06d7a19a1b870ab45cbd346310266fdcb54885c
scottmotion <scott@scottmotion.com>
2023-07-13 08:35:46 -0700

    Update AppHeader.jsx

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -4,7 +4,7 @@ export default function AppHeader() {
         <div className="header__app-brand"><img className="header__app-logo" /></div>
         <div className="header__nav">
           <h1 className="header__board-title">Board Title</h1>
-          <button>Button</button>
+          <button>+ Add New Task</button>
         </div>
       </header>
     )
