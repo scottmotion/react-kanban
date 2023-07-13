67daae3c6ff4aa1b724a9a2c7a0c501ced421c1d
scottmotion <scott@scottmotion.com>
2023-07-12 21:37:41 -0700

    scroll bar gap

--- src/Components/BoardColumns.jsx
+++ src/Components/BoardColumns.jsx
@@ -1,31 +1,93 @@
 export default function BoardColumns(props) {
 
-  let className = 'board__columns'
+  let className = 'board__column-section'
 
   return (
     <>
       <section className={className}>
-        <div className="board__column-single">Board Column 1
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
-          <div className="task-card">Task Card</div>
+        <div className="board__column-single--wrapper">
+          <div className="board__column-title">Board Column 1</div>
+          <div className="board__column-single">
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+          </div>
         </div>
-        <div className="board__column-single">Board Column 2</div>
-        <div className="board__column-single">Board Column 3</div>
-        <div className="board__column-single">Board Column 4</div>
+
+        <div className="board__column-single--wrapper">
+          <div className="board__column-title">Board Column 2</div>
+          <div className="board__column-single">
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+          </div>
+        </div>
+
+        <div className="board__column-single--wrapper">
+          <div className="board__column-title">Board Column 3</div>
+          <div className="board__column-single">
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+          </div>
+        </div>
+
+        <div className="board__column-single--wrapper">
+          <div className="board__column-title">Board Column 4</div>
+          <div className="board__column-single">
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+            <div className="task-card">Task Card</div>
+          </div>
+        </div>
+
       </section>
     </>
   )
