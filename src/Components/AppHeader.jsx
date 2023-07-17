7016f6742b97d45951e9eb61c073fa9f45108066
scottmotion <scott@scottmotion.com>
2023-07-16 20:12:19 -0700

    Update AppHeader.jsx
    refactor

--- src/Components/AppHeader.jsx
+++ src/Components/AppHeader.jsx
@@ -5,7 +5,7 @@ import './AppHeader.css'
 
 export default function AppHeader(props) {
 
-  const [showEditBoardMenu, setShowEditBoardMenu] = useState(false)
+  const [showBoardOptions, setShowBoardOptions] = useState(false)
 
   let headerClassName = "header"
   if (props.darkMode) {
@@ -14,15 +14,18 @@ export default function AppHeader(props) {
     headerClassName += " light-mode"
   }
 
-  let editBoardMenuClassName = "edit-board-menu"
+  function addNewTask() {
 
-  if (showEditBoardMenu) {
-    editBoardMenuClassName += " show"
-  } else {
-    editBoardMenuClassName += " hide"
   }
-  function toggleEditBoardMenu() {
-    setShowEditBoardMenu(prevState => !prevState)
+
+  let boardOptionsClassName = "edit-board-menu"
+  if (showBoardOptions) {
+    boardOptionsClassName += " show"
+  } else {
+    boardOptionsClassName += " hide"
+  }
+  function toggleBoardOptions() {
+    setShowBoardOptions(!showBoardOptions)
   }
 
   return (
@@ -31,20 +34,19 @@ export default function AppHeader(props) {
       <div className="header__nav">
         <h1 className="header__board-title">Board Title</h1>
         <div className="header__button-wrapper">
-          <button className="button header-button header-button--new-task">
+          <button className="button header-button header-button--new-task" onClick={addNewTask}>
             <PlusIcon className='header-button__icon header-button__icon--new'/>
             <div className="header-button__text--new-task">Add New Task</div>
           </button>
           <div className="edit-board-menu--wrapper">
-            <button className="button header-button header-button--edit-board" onClick={toggleEditBoardMenu}>
+            <button className="button header-button header-button--edit-board" onClick={toggleBoardOptions}>
               <VerticalEllipsisIcon className='header-button__icon header-button__icon--edit'/>
             </button>
-            <div className={editBoardMenuClassName}>
+            <div className={boardOptionsClassName}>
               <div className="button edit-board-menu--edit">Edit Board</div>
               <div className="button edit-board-menu--delete">Delete Board</div>
             </div>            
           </div>
-
         </div>
       </div>
     </header>
