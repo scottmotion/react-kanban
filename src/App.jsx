e1777d4e79b24dd4d7cc1e5d1ed6fde667e18d34
scottmotion <scott@scottmotion.com>
2023-07-17 11:35:55 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import './App.css'
 import Sidebar from './Components/Sidebar'
@@ -7,9 +7,13 @@ import BoardWrapper from './Components/BoardWrapper'
 import AppHeader from './Components/AppHeader'
 import ColumnsWrapper from "./Components/ColumnsWrapper"
 
+import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore"
+import { boardsCollection, db } from "./firebase"
+
 function App() {
   const [sidebarVisible, setSidebarVisible] = useState(true)
   const [darkMode, setDarkMode] = useState(true)
+  const [boards, setBoards] = useState([])
 
   function toggleDarkMode() {
     setDarkMode(prevMode => !prevMode)
@@ -27,6 +31,18 @@ function App() {
   function showSidebar() {
     setSidebarVisible(true)
   }
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(boardsCollection, function(snapshot) {
+        // Sync up our local notes array with the snapshot data
+        const boardsArr = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            id: doc.id
+        }))
+        setBoards(boardsArr)
+    })
+    return unsubscribe
+  }, [])
 
   return (
     <>
