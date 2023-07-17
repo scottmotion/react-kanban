fb38a4f5459e6bbae929df544f1988cfea8218a3
scottmotion <scott@scottmotion.com>
2023-07-17 11:35:57 -0700

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { collection, getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
+import { getFirestore, collection } from "firebase/firestore"
+
 const firebaseConfig = {
   apiKey: "AIzaSyAkBafSn3g8LnR4gQUZcS_WhgVQZ_w3D-I",
   authDomain: "react-kanban-37add.firebaseapp.com",
@@ -14,7 +11,6 @@ const firebaseConfig = {
   appId: "1:666593534499:web:9baae7efd58f2dda65c796"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const boardsCollection = collection(db, "boards")
