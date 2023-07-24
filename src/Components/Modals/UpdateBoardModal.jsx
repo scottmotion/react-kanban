62866f4a763f9f4c87d90ef7f39d4417b75add62
scottmotion <scott@scottmotion.com>
2023-07-24 14:59:51 -0700

    Update UpdateBoardModal.jsx
    remove columns from tempColumns state

--- src/Components/Modals/UpdateBoardModal.jsx
+++ src/Components/Modals/UpdateBoardModal.jsx
@@ -2,25 +2,31 @@ import { useState } from "react";
 import styles from "./Modal.module.css"
 
 export default function UpdateBoardModal(props) {
+
     const [tempBoard, setTempBoard] = useState({
         name: props.currentBoard.name
     })
 
     const [tempColumns, setTempColumns] = useState(props.columns)
-    console.log("updateBoard columns: ", tempColumns)
+    console.log("tempColumns: ", tempColumns)
+
+    const [newColumns, setNewColumns] = useState([])
 
     const tempColumnInputs = tempColumns.map((column, index) => (
-      <label className={styles.modalFormLabel} key={index}>
-        <input
-          type="text"
-          placeholder="Column Name"
-          className={styles.modalFormInput}
-          name="columnName"
-          value={column.name || ""}
-          onChange={handleChangeColumn}
-          id={column.id}
-        />
-      </label>
+      <div className={styles.modalFormInputWrapper} key={index}>
+        <label className={styles.modalFormLabel}>
+          <input
+            type="text"
+            placeholder="Column Name"
+            className={styles.modalFormInput}
+            name="columnName"
+            value={column.name || ""}
+            onChange={(e) => handleChangeColumn(e, index)}
+            id={column.id}
+          />
+        </label>
+        <button className={`${styles.btn} ${styles.deleteBtn}`} onClick={(e) => {handleRemoveColumn(e, index)}}>X</button>
+      </div>
     ))
   
     let modalClassName = "modal"
@@ -38,15 +44,27 @@ export default function UpdateBoardModal(props) {
         }))
     }
 
-    function handleChangeColumn(event) {
+    function handleNewColumn(event) {
+      event.preventDefault();
+      console.log("New Column Clicked")
+      setTempColumns([
+        ...tempColumns,
+        {
+          name: '',
+          order: tempColumns.length
+        }
+      ])
+    }
+
+    function handleChangeColumn(event, columnIndex) {
       const columnValue = event.target.value
-      const columnId = event.target.id
-      const tempColumn = {id:columnId, name: columnValue}
+      // const columnId = event.target.id
+      const tempColumn = {name: columnValue}
 
       console.log("tempColumn: ", tempColumn)
   
-      setTempColumns(tempColumns.map(c => {
-        if (c.id === columnId) {
+      setTempColumns(tempColumns.map((c, index) => {
+        if (index === columnIndex) {
           return tempColumn;
         } else {
           return c;
@@ -57,14 +75,20 @@ export default function UpdateBoardModal(props) {
     function handleRemoveColumn(event, columnIndex) {
       event.preventDefault();
       console.log("Remove Column Clicked: ", columnIndex)
-      // setNewColumns(
-      //   newColumns.filter((c, index) =>
-      //     index !== columnIndex
-      //   )
-      // );
+      const filteredColumns = tempColumns.filter((c, index) =>
+        index !== columnIndex
+      )
+      const newArray = filteredColumns.map((c, index) => ({
+        ...c,
+        order: index
+      }))
+
+      setTempColumns(
+        newArray
+      );
     }
 
-    const handleSubmit = (event) => { //TODO: Send temp columns
+    const handleSubmit = (event) => { //TODO: Check old columns against temp columns
         event.preventDefault();
         if (tempBoard.name) {
           props.updateBoard(tempBoard)
@@ -97,9 +121,9 @@ export default function UpdateBoardModal(props) {
                   </fieldset> 
                 : ""          
               }
-
+              <button className={`${styles.btn} ${styles.addBtn}`} onClick={(e) => handleNewColumn(e)}>+ Add New Column</button>
               <button className={`${styles.btn} ${styles.saveBtn}`} type="submit">Save</button>
-              <button className={`${styles.btn} ${styles.cancelBtn}`} onClick={(e) => {e.preventDefault(); props.setModalOpen("")}}>Cancel</button>
+              {/* <button className={`${styles.btn} ${styles.cancelBtn}`} onClick={(e) => {e.preventDefault(); props.setModalOpen("")}}>Cancel</button> */}
             </form>
           </div>
         </div>
