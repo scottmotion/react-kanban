22ddddbb31e5ba3750bbac66dcea5c0af9ffd4aa
scottmotion <scott@scottmotion.com>
2023-08-10 16:21:56 -0700

    Update firebase.js
    add recaptcha v3 app check
    add debug token for localhost in env.dev

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 import { getFirestore, collection } from "firebase/firestore"
 
@@ -12,6 +13,20 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+// App Chck debug token for use on localhost only
+// DO NOT EXPOSE TO PROD
+self.FIREBASE_APPCHECK_DEBUG_TOKEN = import.meta.env.DEV.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN;
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 const db = getFirestore(app)
 const boardsCollection = collection(db, "boardsNew")
 
