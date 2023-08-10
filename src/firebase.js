b5c1cf44569756ea4701d45b2bc59dc248602356
scottmotion <scott@scottmotion.com>
2023-08-10 16:27:17 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -16,13 +16,11 @@ const app = initializeApp(firebaseConfig);
 
 // App Chck debug token for use on localhost only
 self.FIREBASE_APPCHECK_DEBUG_TOKEN = import.meta.env.DEV.VITE_FIREBASE_APPCHECK_DEBUG_TOKEN;
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
+// Pass your reCAPTCHA v3 site key (public key) to activate()
 const appCheck = initializeAppCheck(app, {
   provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
 
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
+  // Optional argument. If true, the SDK automatically refreshes App Check tokens as needed.
   isTokenAutoRefreshEnabled: true
 });
 
