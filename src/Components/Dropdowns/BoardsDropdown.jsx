3a51b252589723a5a425933cc9d3f3827ee3153c
scottmotion <scott@scottmotion.com>
2023-08-14 15:06:48 -0700

    Update BoardsDropdown.jsx

--- src/Components/Dropdowns/BoardsDropdown.jsx
+++ src/Components/Dropdowns/BoardsDropdown.jsx
@@ -1,24 +1,30 @@
+import { useEffect, useState } from 'react'
+
 import './BoardsDropdown.css'
 
-export default function BoardsDropdown({boards, currentBoardId, setCurrentBoardId}) {
+export default function BoardsDropdown({ boards, currentBoardId, setCurrentBoardId }) {
 
-    const currentBoard = boards.find(board => board.id === currentBoardId) || boards[0]
+    const [currentBoard, setCurrentBoard] = useState(boards[0])
+
+    useEffect(() => {
+        setCurrentBoard(boards.find(board => board.id === currentBoardId) || boards[0])
+    }, [currentBoardId])
 
     const boardsDropdownOptions = boards.map((board, index) => (
         <option className="dropdownOption" key={index} value={board.id}>{board.name}</option>
-    )) 
+    ))
 
     function handleChangeBoard(event) {
-    const {value} = event.target
-    setCurrentBoardId(value)
+        const { value } = event.target
+        setCurrentBoardId(value)
     }
 
     return (
-        <label className = "dropdownLabel">
+        <label className="dropdownLabel">
             <select
                 className="dropdownSelect"
                 name="board"
-                value={currentBoard.name}
+                value={currentBoard.id}
                 onChange={handleChangeBoard}
             >
                 {boardsDropdownOptions}
