387e4f9df335560687fb45c330c8c1aacedfff3c
scottmotion <scott@scottmotion.com>
2023-08-14 13:00:12 -0700

    Update NewTaskButton.jsx

--- src/Components/Buttons/NewTaskButton.jsx
+++ src/Components/Buttons/NewTaskButton.jsx
@@ -1,9 +1,9 @@
 import { ReactComponent as PlusIcon } from "/src/assets/icons/icon-plus.svg"
 import './NewTaskButton.css'
 
-export default function NewTaskButton(props) {
+export default function NewTaskButton({handleNewTask, disabled}) {
     return (
-        <button className="button button--new-task" onClick={props.handleNewTask} disabled={props.disabled}>
+        <button className="button button--new-task" onClick={handleNewTask} disabled={disabled}>
             <PlusIcon className='button__icon button__icon--new'/>
             <div className="button__text--new-task">Add New Task</div>
         </button>
