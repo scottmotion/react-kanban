88d3256350fc7d1cb23f10abe8c67d87ef70f1c3
scottmotion <scott@scottmotion.com>
2023-08-17 11:38:10 -0700

    new files

---
+++ src/Components/Dropdowns/MobileBoardsDropdown.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+
+import './MobileBoardsDropdown.css'
+
+export default function MobileBoardsDropdown({ boards, currentBoardId, setCurrentBoardId }) {
+
+    const [currentBoard, setCurrentBoard] = useState(boards[0])
+
+    useEffect(() => {
+        setCurrentBoard(boards.find(board => board.id === currentBoardId) || boards[0])
+    }, [currentBoardId])
+
+    const boardsDropdownOptions = boards.map((board, index) => (
+        <option className="dropdownOption" key={index} value={board.id}>{board.name}</option>
+    ))
+
+    function handleChangeBoard(event) {
+        const { value } = event.target
+        setCurrentBoardId(value)
+    }
+
+    return (
+        <label className="dropdownLabel">
+            <select
+                className="dropdownSelect"
+                name="board"
+                value={currentBoard.id}
+                onChange={handleChangeBoard}
+            >
+                {boardsDropdownOptions}
+            </select>
+        </label>
+    )
+}
